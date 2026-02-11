b7e9f58eb2964e5dec1f2e6a741e6bc4a6501f48
KAVYA <kavyas.23csd@kongu.edu>
2026-02-11 14:57:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "game_server" {
-  ami           = "ami-019715e0d74f695be" 
-  instance_type = "t3.micro"
-  key_name      = "jenkinskkp"
+  ami           = "ami-073130f74f5ffb161" 
+  instance_type = "t3.small"
+  key_name      = "jenkin2"
 
   security_groups = [aws_security_group.game-sg-jenkins.name]
 
